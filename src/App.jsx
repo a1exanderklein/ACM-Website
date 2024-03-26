90c685656b1f763cb14ef1bb166e12f2e4499627
j10 <jasontenczar@gmail.com>
2024-03-25 23:30:43 -0400

    added animated about section

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,14 @@ import Menu from './components/Menu';
 import About from './components/About';
 import SponsorCarousel from './components/SponsorCarousel';
 import SIGs from './components/SIGs';
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 
 function App() {
+
+  const { scrollYProgress } = useViewportScroll();
+  const y = useTransform(scrollYProgress, [0, 1], [0, -500]);
+
+
   return (
     <>
         <div className='splash-container'>
@@ -16,7 +22,13 @@ function App() {
           <Menu/>
         </div>
         <Home/>
-        <About id='about'/>
+
+        {/* <motion.div style={{ y }} className='about-container'>
+          <About id='about'/> 
+        </motion.div> */}
+
+        <About id='about'/> 
+        
         <h1 id='sponsors' className='flex justify-center p-4'>Sponsors</h1>
         <SponsorCarousel id='sponsors'/>
         <SIGs/>
