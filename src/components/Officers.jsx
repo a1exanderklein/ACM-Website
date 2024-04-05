09b0a28244b73b1612a53d551746161a451cbd3a
alexfisher03 <fisherr.alexander@gmail.com>
2024-04-05 17:00:15 -0400

    Add Officers swiper feature

--- src/components/Officers.jsx
+++ src/components/Officers.jsx
@@ -1,8 +1,11 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import { EffectCards } from 'swiper/modules';
+import { FaLinkedin } from "react-icons/fa";
+import 'swiper/css/pagination';
 
 function Officers() {
     const officers = [
@@ -11,108 +14,168 @@ function Officers() {
             position: "President",
             linkedin: "https://www.linkedin.com/in/victorpolisetty/",
             imgSrc: require('../assets/headshots/Victor+Headshots.jpg'),
+            bio: "Victor Bio",
         },
         {
             name: "Norman Bukingolts",
             position: "External Vice President",
             linkedin: "https://www.linkedin.com/in/norman-codes/",
             imgSrc: require('../assets/headshots/Norman+Headshot.jpg'),
+            bio: "Norman Bio",
+
         },
         {
             name: "Courtney Giang",
             position: "Internal Vice President",
             linkedin: "https://www.linkedin.com/in/courtney-giang/",
             imgSrc: require('../assets/headshots/Courtney+Headshot.jpg'),
+            bio: "Courtney Bio",
+
         },
         {
           name: "Alexander Klein",
           position: "Secretary",
           linkedin: "https://www.linkedin.com/in/alexanderkleincs/",
           imgSrc: require('../assets/headshots/Alexander+Headshot.jpg'),
+          bio: "Alex Bio",
+
       },
       {
           name: "Shreyas Kodela",
           position: "Treasurer",
           linkedin: "https://www.linkedin.com/in/shreyaskodela/",
           imgSrc: require('../assets/headshots/Shreyas-headshot.jpg'),
+          bio: "Shreyas Bio",
+
       },
       {
           name: "Maya Harris",
           position: "Marketing Lead",
           linkedin: "https://www.linkedin.com/in/mjoyharris/",
           imgSrc: require('../assets/headshots/Maya+Headshot.jpg'),
+          bio: "Maya Bio",
+
       },
       {
           name: "Jack Crew",
           position: "Professional Affairs Lead",
           linkedin: "https://www.linkedin.com/in/jack-crew/",
           imgSrc: require('../assets/headshots/Jack+Headshot.jpg'),
+          bio: "Jack Bio",
+
       },
       {
           name: "Andrew Ruales",
           position: "Workshop Lead",
           linkedin: "https://www.linkedin.com/in/andrewruales/",
           imgSrc: require('../assets/headshots/Andrew+Headshot.jpg'),
+          bio: "Andrew Bio",
+
       },
       {
           name: "Miguel Jara",
           position: "Workshop Lead",
           linkedin: "https://www.linkedin.com/in/miguel-jar/",
           imgSrc: require('../assets/headshots/Miguel+Headshot.jpg'),
+          bio: "Miguel Bio",
+
       },
       {
           name: "Connor Munjed",
           position: "Social Lead",
           linkedin: "https://www.linkedin.com/in/connor-munjed-58a171289/",
           imgSrc: require('../assets/headshots/Connor+Headshot.jpg'),
+          bio: "Connor Bio",
+
       },
       {
         name: "Jacob Hoppenstedt",
         position: "Social Lead",
         linkedin: "https://www.linkedin.com/in/jacob-hoppenstedt/",
         imgSrc: require('../assets/headshots/Jacob+Headshot.jpg'),
+        bio: "Jacob Bio",
+
     },
     {
         name: "Jonathan Zhang",
         position: "Mentorship Lead",
         linkedin: "https://www.linkedin.com/in/jonathanzhang53/",
         imgSrc: require('../assets/headshots/Jonathan+Headshot.png'),
+        bio: "Jonathan Bio",
+
     },
     {
         name: "Surya Karthikeyan Vijayalakshmi",
         position: "Graphic Design Lead",
         linkedin: "https://www.linkedin.com/in/kvsurya/",
         imgSrc: require('../assets/headshots/Surya+Headshot.jpg'),
+        bio: "Surya Bio",
+
     },
     {
         name: "Justin Adam",
         position: "Opportunity Lead",
         linkedin: "https://www.linkedin.com/in/justin-adam/",
         imgSrc: require('../assets/headshots/Justin+Headshot.png'),
+        bio: "Justin Bio",
+
     },
     ];
 
+    const [activeOfficer, setActiveOfficer] = useState(officers[0]);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 640 || window.innerHeight < 632);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 640 || window.innerHeight < 632);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const containerClass = isMobile ? 'flexMobile' : 'flexDefault';
+
+
     return (
-        <div className='bg-[#18191A]'>
-            <h1 className="text-center text-white text-3xl py-8">Officers</h1>
-            <Swiper
-                effect={'cards'}
-                grabCursor={true}
-                modules={[EffectCards]}
-                className="mySwiper">
-                {officers.map((officer, index) => (
-                    <SwiperSlide key={index}>
-                        <div className='flex flex-col items-center p-4'>
-                            <img src={officer.imgSrc} alt={officer.name} className='w-32 h-32 rounded-full object-cover mb-4'/>
-                            <h2 className='text-lg text-white'>{officer.name}</h2>
-                            <p className='text-white'>{officer.position}</p>
-                            <a href={officer.linkedin} className='text-blue-500 hover:text-blue-700 mt-2'>
-                                LinkedIn 
-                            </a>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+        
+        <div className='officerCardBg'>
+            <div className={containerClass}>
+                <div className="bg-[#00000033] sm:w-1/2 flex-col items-center justify-center">
+                    <h1 className="text-center text-white text-4xl py-8">Meet The Officers</h1>
+                    <div className="text-center">
+                            <h2 className="text-3xl gradient-text1 p-3">{activeOfficer.name}</h2>
+                            <p className="text-2xl gradient-text2 p-3">{activeOfficer.position}</p>
+                            <p className="dm-mono text-sm p-3 sm:text-md md:text-lg gradient-text3">{activeOfficer.bio}</p>
+                    </div>
+                </div>
+                <div className="pt-10 pb-10 sm:w-1/2 ">
+                    <div className="translate-x-16 sm:translate-x-32">
+                        <Swiper
+                            pagination={true}
+                            effect={'cards'}
+                            grabCursor={true}
+                            modules={[EffectCards]}
+                            className="mySwiper"
+                            spaceBetween={30}
+                            onSlideChange={(swiper) => setActiveOfficer(officers[swiper.activeIndex])}>   
+                            {officers.map((officer, index) => (
+                                <SwiperSlide key={index}>
+                                    <div className='flex flex-col items-center p-4 officerCard'>
+                                        <img src={officer.imgSrc} alt={officer.name} className='w-32 h-32 rounded-full object-cover mb-4'/>
+                                        <h2 className='text-lg text-white text-center'>{officer.name}</h2>
+                                        <a href={officer.linkedin} className='text-3xl text-blue-500 hover:text-blue-700 hover:scale-110 duration-300 mt-2'>
+                                            <FaLinkedin/>
+                                        </a>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
