cac5f3a67876554cc0c77151f3a4cf1469728fed
j10 <jasontenczar@gmail.com>
2024-08-29 14:49:29 -0400

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,21 @@
 import './App.css';
-import NavBar from './components/NavBar';
-import TitlePane from './components/TitlePane';
-import Gradient from './assets/mesh-gradient.png';
-import Home from './components/Home';
-import Menu from './components/Menu';
-import About from './components/About';
-import SponsorCarousel from './components/SponsorCarousel';
-import SIGs from './components/SIGs';
-import { motion, useViewportScroll, useTransform } from "framer-motion";
-import Footer from './components/Footer';
-import Officers from './components/Officers';
-import ACMFish from './assets/acmlogo.png';
-import { useState, useEffect } from 'react';
+import Landing from './Landing';
+import Checkin from './Checkin';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 
 function App() {
 
-  const [ isSmall, setIsSmall ] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmall(window.innerWidth < 1100);
-    };
-  
-    // Call handleResize once to set the initial state correctly
-    handleResize();
-  
-    window.addEventListener('resize', handleResize);
-  
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
- 
 
   return (
     <>
-        <div className='splash-container' id='home'>
-          {isSmall && <span className='fish-conatiner'>
-             <img src={ACMFish} alt='ACM' className='fish'/>
-          </span>}
-          
-          <TitlePane/>
-          <Menu/>
-        </div>
-        <Home/>
-        <About/> 
-        <Officers/>
-        <h1 id='sponsors' className='flex justify-center p-4'>Sponsors</h1>
-        <SponsorCarousel id='sponsors'/>
-        <SIGs id='sigs'/>
-        <Footer/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/checkin' element={<Checkin/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
